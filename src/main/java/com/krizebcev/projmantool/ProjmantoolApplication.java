db36ebc19ee539e0b1ab86f6a036f3e20c7a60a8
krizebcev <krizebcev@foi.hr>
2019-10-19 00:51:53 +0200

    first commit

---
+++ src/main/java/com/krizebcev/projmantool/ProjmantoolApplication.java
@@ -0,0 +1,13 @@
+package com.krizebcev.projmantool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjmantoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjmantoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krizebcev/projmantool/ProjmantoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krizebcev.projmantool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjmantoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
