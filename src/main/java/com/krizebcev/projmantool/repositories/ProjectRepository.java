40aac43785e358bb50aba1d2a075ad46756881bf
krizebcev <krizebcev@foi.hr>
2019-10-19 15:48:12 +0200

    Created ProjectController, ProjectService and ProjectRepository

---
+++ src/main/java/com/krizebcev/projmantool/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.krizebcev.projmantool.repositories;
+
+import com.krizebcev.projmantool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+}
---
+++ src/main/java/com/krizebcev/projmantool/services/ProjectService.java
@@ -0,0 +1,17 @@
+package com.krizebcev.projmantool.services;
+
+import com.krizebcev.projmantool.domain.Project;
+import com.krizebcev.projmantool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        return projectRepository.save(project);
+    }
+}
